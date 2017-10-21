5e62619f40ac74cdc921492da8a5ba05207826aa
CatAfanacy <catafanacy@gmail.com>
2017-10-22 00:30:37 +0400

    Commit 2.
    Added entry point

---
+++ src/PO51/Golubcov/wdad/Application.java
@@ -0,0 +1,7 @@
+package PO51.Golubcov.wdad;
+
+public class Application {
+    public static void main(String[] ars){
+        System.out.println("I`m Golubcov Vladimir, and I`m not a monkey (False)");
+    }
+}
